50d8abe4f1ccf208e4b52f1a2542e7b8624509f8
phara <sopharakeat@gmail.com>
2024-08-10 19:42:39 +0700

    first commit too

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import {NgOptimizedImage} from "@angular/common";
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent, NgOptimizedImage,  RouterModule,],
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+      <header class="flex items-end space-x-2">
+        <img class="h-10 w-10" [src]="'https://img.icons8.com/?size=40&id=21081&format=png'" alt="logo" aria-hidden="true">
+        <h1 class="text-2xl font-bold text-blue-900">Homes</h1>
+
+      </header>
+      </a>
+
+      <section class="content">
+
+        <router-outlet></router-outlet>
+      </section>
+
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'FirstAgProject';
+  title = 'homes';
 }
